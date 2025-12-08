5e7f04d8dae04d3dfb43d1d532bdd176246ad606
Arbik26 <jakub.uzar2@gmail.com>
2025-12-08 20:55:54 +0100

    Add missing CRUD methods to MongoDbService

--- Services/MongoDbService.cs
+++ Services/MongoDbService.cs
@@ -1,3 +1,4 @@
+
 using MongoDB.Driver;
 using CrudApp.Models;
 
@@ -18,5 +19,48 @@ namespace CrudApp.Services
         {
             return _database.GetCollection<Product>("products");
         }
+
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+            var collection = GetProductsCollection();
+            return await collection.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(string id)
+        {
+            var collection = GetProductsCollection();
+            var objectId = MongoDB.Bson.ObjectId.Parse(id);
+            return await collection.Find(p => p.Id == objectId).FirstOrDefaultAsync();
+        }
+
+        public async Task<string> CreateProductAsync(Product product)
+        {
+            var collection = GetProductsCollection();
+            await collection.InsertOneAsync(product);
+            return product.Id.ToString();
+        }
+
+        public async Task<bool> UpdateProductAsync(string id, Product product)
+        {
+            var collection = GetProductsCollection();
+            var objectId = MongoDB.Bson.ObjectId.Parse(id);
+            var filter = Builders<Product>.Filter.Eq(p => p.Id, objectId);
+            var update = Builders<Product>.Update
+                .Set(p => p.Name, product.Name)
+                .Set(p => p.Description, product.Description)
+                .Set(p => p.Price, product.Price);
+            var result = await collection.UpdateOneAsync(filter, update);
+            return result.ModifiedCount > 0;
+        }
+
+        public async Task<bool> DeleteProductAsync(string id)
+        {
+            var collection = GetProductsCollection();
+            var objectId = MongoDB.Bson.ObjectId.Parse(id);
+            var filter = Builders<Product>.Filter.Eq(p => p.Id, objectId);
+            var result = await collection.DeleteOneAsync(filter);
+            return result.DeletedCount > 0;
+        }
     }
 }
+
