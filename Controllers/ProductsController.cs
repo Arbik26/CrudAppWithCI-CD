924d98b6642db7a580f4376b7785f934b771a31b
Arbik26 <jakub.uzar2@gmail.com>
2025-12-08 20:44:48 +0100

    Fix: Add all CRUD methods to MongoDbService

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -129,14 +129,15 @@ public class ProductsController : ControllerBase
 
 public class CreateProductRequest
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
     public decimal Price { get; set; }
 }
 
 public class UpdateProductRequest
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
     public decimal Price { get; set; }
 }
+
