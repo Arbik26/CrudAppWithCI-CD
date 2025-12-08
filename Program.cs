d44131ea5a6ab0176b2a3765d49a0238350e3096
Arbik26 <jakub.uzar2@gmail.com>
2025-12-08 20:09:56 +0100

    Update Program.cs for MongoDB and CORS

--- Program.cs
+++ Program.cs
@@ -1,23 +1,24 @@
+using MongoDB.Driver;
 using CrudApp.Services;
 
-var builder = WebApplicationBuilder.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-// Dodaj CORS
+// MongoDB
+var mongoConnectionString = builder.Configuration["mongodb+srv://JakubUzar:HasloDB123>@clusteruzar.8ukgqsw.mongodb.net/?appName=ClusterUzar"];
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoConnectionString));
+builder.Services.AddScoped<MongoDbService>();
+
+// Serwisy
+builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-    });
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+    );
 });
-
-builder.Services.AddControllers();
-builder.Services.AddScoped<MongoDbService>();
 
 var app = builder.Build();
 
 app.UseCors("AllowAll");
-app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
